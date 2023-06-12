d2ea097c203fd8de083851068fcfe708375b9f53
William <zhijian.yap@newsoft.com.my>
2023-06-12 20:12:09 +0800

    Add project files.

---
+++ APA.BO.V1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APA.BO.V1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
